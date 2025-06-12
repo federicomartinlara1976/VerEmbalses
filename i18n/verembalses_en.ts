4cc3f130ada54b9f4374be49edef2c8f246588d4
Federico Martín Lara <federico@horizontefinal>
2025-06-12 11:22:30 +0200

    Preparado para traducciones

---
+++ i18n/verembalses_en.ts
@@ -0,0 +1,271 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>DialogProgreso</name>
+    <message>
+        <source>Dialog</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>--</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>DialogSelectFecha</name>
+    <message>
+        <source>Seleccionar rango de fechas</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Zona</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Embalse</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Desde:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Hasta:</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>DialogSelectGrafico</name>
+    <message>
+        <source>Tipo de gráfico</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Linear por fecha</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Relación Nivel - Volumen</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Relación Nivel - Porcentaje</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Relación Volumen - Porcentaje</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>DialogShowEmbalsesGraphic</name>
+    <message>
+        <source>Mostrar gráfico</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Tipo de gráfico</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Global</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Detalles</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Imprimir</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Cerrar</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>DialogShowLineGraphic</name>
+    <message>
+        <source>Mostrar gráfico</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Dato Y</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Imprimir</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Cerrar</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>DialogShowSimpleGraphic</name>
+    <message>
+        <source>Mostrar gráfico</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Imprimir</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Cerrar</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>DialogShowTable</name>
+    <message>
+        <source>Datos por fecha</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>TextLabel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Capacidad (Hm3)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>--</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>MEN (m.s.n.m.)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Desde</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Hasta</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Exportar a CSV</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Ver gráfico</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Settings</name>
+    <message>
+        <source>Servidor:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Base de datos:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Zona inicial:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Embalse inicial:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Decimales:</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>VerEmbalses</name>
+    <message>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Embalses</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Zona</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Estadísticas por zona</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>--</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Ver embalses</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Niveles (Hm3)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Media</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Mínimo</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Máximo</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Volumen (Hm3)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Total</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Total capacidad</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Porcentaje %</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>EMBALSE</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Capacidad (Hm3)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Ver gráfico</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Exportar a CSV</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Pluviometría</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Punto de control</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
